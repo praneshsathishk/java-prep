4cb4b78ab23621dfe19b32a2635a019c45230cdd
praneshsathishk <praneshsathishk@gmail.com>
2025-07-05 20:17:01 -0400

    Commit 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,12 @@ public class HelloWorld {
         HelloWorld numAdd = new HelloWorld();
         System.out.println(intSquare(5));
     }
+
     public Integer intAdd(Integer num1, Integer num2) {
         Integer num3 = num1 + num2;
         return num3;
     }
+    
     public static Integer intSquare(Integer num5) {
         Integer num6 = num5 * num5;
         return num6;
